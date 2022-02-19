93de629f55b0b6c6f25f4dd6a2adbff67b05f4d7
Jhol <galilleu2011@gmail.com>
2022-02-19 14:25:19 -0300

    Create pages Login and Cadastro UI

---
+++ app/src/main/java/com/example/soutsoftappjava/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.soutsoftappjava;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.soutsoftappjava.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.textCadastro.setOnClickListener(view -> {
+            startActivity(new Intent(this, CadastrarActivity.class));
+        });
+    }
+}
